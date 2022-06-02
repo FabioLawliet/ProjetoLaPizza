5471a905ce6fc7ec530485f251e6162c3a7fa9be
LUHIZTOMBINI\luhiz <lratombini@gmail.com>
2022-06-01 21:30:35 -0300

    Criação Form base para Cadastro Cliente

---
+++ Views/FormRecCliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LaPizza.Views
+{
+    public partial class FormRecCliente : LaPizza.Views.FormBaseCadastros
+    {
+        public FormRecCliente()
+        {
+            InitializeComponent();
+        }
+    }
+}
